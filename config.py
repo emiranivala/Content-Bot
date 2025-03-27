608964c73bed42bd9734146faadb589f8e2e73b7
emiranivala <taitufam@gmail.com>
2025-03-27 10:06:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,18 +17,18 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = os.getenv("API_ID", "")
-API_HASH = os.getenv("API_HASH", "")
+API_ID = os.getenv("API_ID", "22642292")
+API_HASH = os.getenv("API_HASH", "4502d35191a2fcb02c8467f54789f0ea")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 MONGO_DB = os.getenv("MONGO_DB", "")
-OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split())) # list seperated via space
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "922270982").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
 STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1001234456")) # optional with -100
-FORCE_SUB = int(os.getenv("FORCE_SUB", "-10012345567")) # optional with -100
-MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
-IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
-YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
-INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
-FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002493565037")) # optional with -100
+FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002329264016")) # optional with -100
+MASTER_KEY = os.getenv("MASTER_KEY", None) # for session encryption
+IV_KEY = os.getenv("IV_KEY", None) # for decryption
+YT_COOKIES = os.getenv("YT_COOKIES", None)
+INSTA_COOKIES = os.getenv("INSTA_COOKIES", None)
+FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "20"))
+PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "50000000000000000000000000000000000"))
